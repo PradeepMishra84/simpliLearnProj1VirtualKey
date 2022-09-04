af27b38e6fc66fc3a7369b56aad1b98710aa58f2
PradeepMishra84 <112800608+PradeepMishra84@users.noreply.github.com>
2022-09-04 14:07:22 +0530

    Add files via upload

---
+++ VirtualKeyProject-main/target/src/main/java/org/example/virtualkey/screens/WelcomeScreen.java
@@ -0,0 +1,106 @@
+package org.example.virtualkey.screens;
+
+import org.example.virtualkey.services.DirectoryService;
+import org.example.virtualkey.services.ScreenService;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class WelcomeScreen implements Screen {
+
+    private String welcomeText = "Welcome to VirtualKey(Lockers Pvt. Ltd.)";
+    private String developerText = "Developer: Pradeep Mishra";
+    private String developerMobNo = "Phone no: 8490093765";
+    private String developerEmail_Id = "Email_Id: pradeepmi294@gmail.com";
+
+    private ArrayList<String> options = new ArrayList<>();
+
+
+    public WelcomeScreen() {
+        options.add("1. Show Files");
+        options.add("2. Show File Options Menu");
+        options.add("3. Quit");
+
+    }
+    
+    public void introWS() {
+    	System.out.println(welcomeText);
+        System.out.println(developerText);
+        System.out.println(developerMobNo);
+        System.out.println(developerEmail_Id);
+        System.out.println("\n");
+        Show();
+    }
+    
+    
+    
+    @Override
+    public void Show() {
+    	System.out.println("Main Menu");
+        for (String s : options)  {
+            System.out.println(s);
+        }
+
+    }
+
+    public void GetUserInput() {
+        int selectedOption  = 0;
+        while ((selectedOption = this.getOption()) != 3) {
+            this.NavigateOption(selectedOption);
+        }
+    }
+
+    @Override
+    public void NavigateOption(int option) {
+        switch(option) {
+
+            case 1: // Show Files in Directory
+                this.ShowFiles();
+                
+                this.Show();
+                
+                break;
+                
+            case 2: // Show File Options menu
+            	ScreenService.setCurrentScreen(ScreenService.FileOptionsScreen);
+                ScreenService.getCurrentScreen().Show();
+                ScreenService.getCurrentScreen().GetUserInput();
+                
+                this.Show();
+                
+                break;
+                
+            default:
+                System.out.println("Invalid Option");
+                break;
+        }
+        
+    }
+
+    public void ShowFiles() {
+
+        //TODO: Get the files from the Directory
+    	
+    	//Finished TODO Task
+
+        System.out.println("List of Files: ");
+    	DirectoryService.PrintFiles();
+
+    }
+    
+    private int getOption() {
+        Scanner in = new Scanner(System.in);
+
+        int returnOption = 0;
+        try {
+            returnOption = in.nextInt();
+        }
+        catch (InputMismatchException ex) {
+
+        }
+        return returnOption;
+
+    }
+}
